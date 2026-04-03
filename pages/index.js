500399b0c04a2b5c3ef7160a280154e991afde96
josepelefu-dev <josepelefu@gmail.com>
2026-04-03 22:09:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -162,7 +162,7 @@ export default function Home() {
     padding: "20px",
     borderRadius: "12px",
     whiteSpace: "pre-wrap",
-    lineHeight: "2",
+    lineHeight: type === "dislexia" || type === "tdah" ? "1.5" : "2",
     border: "1px solid #e2e8f0",
     fontFamily: type === "dislexia" ? "OpenDyslexic, Arial" : "Arial",
     fontSize: "17px"
